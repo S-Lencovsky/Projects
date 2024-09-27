ba600fdca0877314714f8abef210d727f7f58993
S-Lencovsky <Shaishi1@Hotmail.com>
2024-09-27 19:27:29 +0100

    added sooner - Form

--- Portfolio project/src/components/contact/Form.jsx
+++ Portfolio project/src/components/contact/Form.jsx
@@ -1,13 +1,53 @@
 "use client"
+import emailjs from '@emailjs/browser';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Toaster, toast } from 'sonner'
 
 export default function Form() {
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
-  console.log(errors);
+
+
+  const sendEmail = (params) => {
+    
+
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        params,
+        {
+          publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY, 
+          limitRate: {
+            throttle: 5000, // You cannot send more than 1 email per 5 seconds
+          }
+        }
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
+
+  const onSubmit = (data) => {
+    const templateParams = {
+      to_name: "Shai Lencovsky",
+      from_name: data.name,
+      reply_to: data.email,
+      message: data.message,
+    }
+    sendEmail(templateParams)
+  };
+  
   
   return (
+    <>
+    <Toaster />
     <form
       onSubmit={handleSubmit(onSubmit)}
       className="max-w-md w-full flex flex-col items-center justify-center space-y-4"
@@ -15,28 +55,50 @@ export default function Form() {
       <input
         type="text"
         placeholder="name"
-        {...register("name", { required: true })}
+        {...register("name", { required: 'This field is required!',
+          minLength: {
+            value: 3,
+            message: "Name must be at least 3 characters long!",
+          }
+
+         })}
         className="w-full p-2 rounded-md shadow-lg text-forground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
       />
+        {
+         errors.name && <span className="inline-block self-start text-accent">{errors.name.message}</span>
+        }
       <input
         type="email"
         placeholder="email"
-        {...register("email", { required: true })}
+        {...register("email", { required: 'This field is required!',
+          minLength: {
+            value: 3,
+            message: "Name must be at least 3 characters long!",
+         } 
+        })}
         className="w-full p-2 rounded-md shadow-lg text-forground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
       />
+      {
+         errors.email && <span className="inline-block self-start text-accent">{errors.email.message}</span>
+        }
+
       <textarea
         placeholder="message"
-        {...register("message", { required: true, max: 256, min: 50 })}
+        {...register("message", { required: 'This field is required!', maxLength: {value: 500, message: "Message must be at most 500 characters long!"}, minLength: {value: 50, message: "Message must be at least 50 characters long!"} })}
         className="w-full p-2 rounded-md shadow-lg text-forground focus:outline-none focus:ring-2 focus:ring-accent/50 custom-bg"
       />
+        {
+         errors.message && <span className="inline-block self-start text-accent">{errors.message.message}</span>
+        }
 
       <input
-        value="Cast your message"
+        value="Cast your message!"
         className="px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop-blur-sm text-forground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize"
         type="submit"
       />
     </form>
+    </>
   );
 }
 
-{/*Stopped at - 2:25:06 - npm run dev */}
+{/* Stopped at -  2:41:45 - npm run dev - Sooner popup */}
