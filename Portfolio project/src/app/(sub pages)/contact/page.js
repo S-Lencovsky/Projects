8d949fd4527e036cebb41fb8af3b896beff63846
S-Lencovsky <Shaishi1@Hotmail.com>
2024-09-27 12:11:51 +0100

    Added Form.jsx

--- Portfolio project/src/app/(sub pages)/contact/page.js
+++ Portfolio project/src/app/(sub pages)/contact/page.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import bg from "../../../../public/background/contact-background.png";
+import Form from "@/components/contact/Form";
+
 
 
 
@@ -14,14 +16,14 @@ export default function Contact() {
 
       <article className="relative w-full flex flex-col items-center justify-center spacey-8">
         <div className="flex flex-col items-center justify-center spacey-6 w-3/4">
-          <h1 className="text-5xl text-accent font-semibold">Contact</h1>
-          <p className="text-lg">Get in touch</p>
+          <h1 className="text-5xl text-accent font-semibold text-center text-4xl capitalize">
+            Contact</h1>
+          <p className="text-center font-light">Embark on a journey of connection! By filling out the form below, you’ll send a message through the mystical ether, allowing your thoughts to reach me. Let’s weave our stories together and create something truly enchanting!</p>
         </div>
+        <Form/>
       </article>
     </>
   );
 
 }
 
-{/*Stopped at - 2:17:52
-  contact page*/}
