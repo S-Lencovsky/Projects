c83cfdd3dee33cb0ee707162365bc3e6635593e4
S-Lencovsky <Shaishi1@Hotmail.com>
2024-09-28 12:17:56 +0100

    stopped - index nav

--- Portfolio project/src/components/navigation/NavButton.jsx
+++ Portfolio project/src/components/navigation/NavButton.jsx
@@ -10,6 +10,7 @@ import {
 } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import ResponsiveComponent from "../responsiveComponent";
 
 const getIcon = (icon) => {
   switch (icon) {
@@ -36,7 +37,13 @@ const getIcon = (icon) => {
 };
 const NavButton = ({ x, y, label, link, icon, newTab }) => {
   return (
-    <div
+    <ResponsiveComponent>
+
+    {({size}) => {
+
+      return size && size >= 480 ? 
+
+      <div
       className="absolute cursor-pointer z-50"
       style={{ transform: `translate(${x}, ${y})` }}
     >
@@ -58,6 +65,39 @@ const NavButton = ({ x, y, label, link, icon, newTab }) => {
         </span>
       </Link>
     </div>
+
+      :
+
+      <div
+      className="cursor-pointer z-50"
+      
+    >
+      <Link
+        href={link}
+        target={newTab ? "_blank" : "_self"}
+        className="text-foreground rounded-full flex items-center justify-center
+        custom-bg"
+        aria-label={label}
+        name={label}
+      >
+        <span className="relative w-14 h-14 p-4 hover:text-accent">
+          {getIcon(icon)}
+          <span className="peer bg-transparent absolute top-0 left-0 w-full h-full" />
+
+          <span className="absolute hidden peer-hover:block px-2 py-1 left-full mx-2 top-1/2 -translate-y-1/2 bg-background text-foreground text-sm rounded-md shadow-lg whitespace-nowrap">
+            {label}
+          </span>
+        </span>
+      </Link>
+    </div>
+    }}
+
+
+
+
+
+
+    </ResponsiveComponent>
   );
 };
 
--- Portfolio project/src/components/navigation/index.jsx
+++ Portfolio project/src/components/navigation/index.jsx
@@ -2,6 +2,7 @@
 import { BtnList } from "@/app/data";
 import React from "react";
 import NavButton from "./NavButton";
+import ResponsiveComponent from "../responsiveComponent";
 
 const Navigation = () => {
   const angleIncrement = 360 / BtnList.length;
@@ -9,21 +10,53 @@ const Navigation = () => {
 
   return (
     <div className="w-full fixed h-screen flex items-center justify-center">
-      <div className="w-max flex items-center justify-center relative hover:pause animate-spin-slow group ">
-        {BtnList.map((btn, index) => {
-          const angleRad = (index * angleIncrement * Math.PI) / 180;
-          const radius = "calc(20vw - 1rem)";
-          const x = `calc(${radius}*${Math.cos(angleRad)})`;
-          const y = `calc(${radius}*${Math.sin(angleRad)})`;
+      <ResponsiveComponent>
+      {({size}) => {
+        
+        return size && size >= 480 ? (
+          <div className="w-max flex items-center justify-center relative hover:pause animate-spin-slow group ">
+            {BtnList.map((btn, index) => {
+              const angleRad = (index * angleIncrement * Math.PI) / 180;
+              const radius = "calc(20vw - 1rem)";
+              const x = `calc(${radius}*${Math.cos(angleRad)})`;
+              const y = `calc(${radius}*${Math.sin(angleRad)})`;
 
-          return <NavButton key={btn.label} x={x} y={y} {...btn} />;
-          
-        })}
-      </div>
+              return <NavButton key={btn.label} x={x} y={y} {...btn} />;
+            })}
+          </div>
+        ) : (
+          <>
+          <div className="w-full px-2.5 xs:px-0 xs:w-max flex flex-col items-start items-center justify-center relative group ">
+            {BtnList.slice(0, BtnList.length/2).map((btn, index) => {
+              const angleRad = (index * angleIncrement * Math.PI) / 180;
+              const radius = "calc(20vw - 1rem)";
+              const x = `calc(${radius}*${Math.cos(angleRad)})`;
+              const y = `calc(${radius}*${Math.sin(angleRad)})`;
+
+              return <NavButton key={btn.label} x={x} y={y} {...btn} />;
+            })}
+          </div>
+
+          <div className="w-max flex items-center justify-center relative group ">
+            {BtnList.slice(BtnList/2, BtnList.length).map((btn, index) => {
+              const angleRad = (index * angleIncrement * Math.PI) / 180;
+              const radius = "calc(20vw - 1rem)";
+              const x = `calc(${radius}*${Math.cos(angleRad)})`;
+              const y = `calc(${radius}*${Math.sin(angleRad)})`;
+
+              return <NavButton key={btn.label} x={x} y={y} {...btn} />;
+            })}
+          </div>
+          </>
+        );
+      }
+      
+      }
+
+        </ResponsiveComponent>
     </div>
   );
 };
 
 export default Navigation;
 
-{/* Stopped at 2:51:28 - npm run dev - adding hooks */}
