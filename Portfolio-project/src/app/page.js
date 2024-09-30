41482bdb477dc84a650684039bbe1a9f04aaad76
S-Lencovsky <Shaishi1@Hotmail.com>
2024-09-30 18:34:19 +0100

    Wizard update

--- Portfolio-project/src/app/page.js
+++ Portfolio-project/src/app/page.js
@@ -11,7 +11,7 @@ const Wizard = dynamic(() => import("@/components/models/Wizard"), { ssr: false 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
-      <Image priority sizes="100vw" src={bg} alt='background-image' fill className="w-full h-full object-cover object-center opacity-25" />
+      <Image priority sizes="100vw" src={bg} alt='background-image' fill className="-z-50 w-full h-full object-cover object-center opacity-25" />
     <div className='w-full h-screen'>
       {/* navigation component and 3d model */}
       <Navigation/>
